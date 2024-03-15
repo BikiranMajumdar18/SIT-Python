3efdc073abf8f78afc3a7a7832b595f427f8486e
BikiranMajumdar18 <163332413+BikiranMajumdar18@users.noreply.github.com>
2024-03-15 19:38:40 +0530

    Update Q2.py

--- Assignment 2/Q2.py
+++ Assignment 2/Q2.py
@@ -1,8 +1,9 @@
-''' The distance between two cities (in km.) is input through the keyboard. Write a program to
-convert and print this distance in meters, feet, inches and centimeters.''' 
+'''Any integer is input through the keyboard. Write a program to find out whether 
+it is an odd number or even number.'''
 
-km = (int(input("Enter the value in km: ")))
-m = km*1000
-print("Distance in meter: ", m)
-ft = km*3280.24
-print("Distance in feet: ", ft)
+n = int(input("Enter the number: "))
+
+if n % 2 == 0:
+    print(n,"is an even number")
+else:
+    print(n,"is an odd number")
